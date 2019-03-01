bb16f0a3b322bdf39744e78a18a3dad3abae29df
SamyakLearner <43209060+SamyakLearner@users.noreply.github.com>
2019-03-01 23:57:35 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,13 +1,39 @@
-console.log('Starting app---------------------------------------------');
+console.log('-----------------------------------Starting app---------------------------------------------');
 
 const fs = require('fs');
-const os = require('os');
+const _ = require('lodash');
+const yargs = require('yargs');
 
-let user = os.userInfo();
-console.log(user);
+const notes = require('./notes.js');
 
-console.log(`hello  ${user.username} `);
-fs.appendFile('greetings.txt',`hello ${user.username} `,function (err){
-    if(err)
-    {console.log('An error occured');}
-});
+const argv = yargs.argv;
+let command = argv._[0];            //taking the command from the user and storing it into a variable,
+                                    //here _ is not lodash variable
+
+console.log('Command :',command);
+
+if(command === 'addNote')
+{
+ notes.addnote(argv.title,argv.body);
+
+}
+
+else if(command === 'readnote')
+{console.log('Reading a note : \n');
+notes.readnote(argv.title);}
+
+else if(command === 'listnotes')
+{console.log('Listing all the notes present previously \n ');
+notes.listnotes();}
+
+else if(command === 'deletenote')
+{console.log('deleting a note \n ');
+notes.deletenote(argv.title);}
+
+else
+{console.log('Command not recognized !!! \n');}
+
+
+
+console.log('-----------------------------------Closing app---------------------------------------------');
+
